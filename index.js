eab25b90dd27371add25335c0da8fd54f6278c94
Praveen Chetkuri <162853531+PraveenChetkuri@users.noreply.github.com>
2024-04-11 19:31:52 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,40 +1,61 @@
 var noOfDrumButtons = document.querySelectorAll(".drum").length;
 
+//below logic is for button press
 for (var i=0; i<noOfDrumButtons; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var letter = this.innerHTML;
-        switch (letter) {
-            case 'w': var audio = new Audio("./sounds/crash.mp3");
-                    audio.play();
-                    break;
-
-            case 'a': var audio = new Audio("./sounds/kick-bass.mp3");
-                    audio.play();
-                    break;
-
-            case 's': var audio = new Audio("./sounds/snare.mp3");
-                    audio.play();
-                    break;
-                    
-            case 'd': var audio = new Audio("./sounds/tom-1.mp3");
-                    audio.play();
-                    break;
-                    
-            case 'j': var audio = new Audio("./sounds/tom-2.mp3");
-                    audio.play();
-                    break;
-                   
-            case 'k': var audio = new Audio("./sounds/tom-3.mp3");
-                    audio.play();
-                    break;
-                   
-            case 'l': var audio = new Audio("./sounds/tom-4.mp3");
-                    audio.play();
-                    break;
-                    
-            default:
-                break;
-        }
+        makeSound(letter);
+        buttonAnimation(letter);
+        
     });
 }
 
+//following function is for keyboard strokes
+        document.addEventListener("keydown", function(event){
+                makeSound(event.key);
+                buttonAnimation(event.key);
+        });
+
+function makeSound(key){
+        switch (key) {
+                case 'p': var audio = new Audio("./sounds/crash.mp3");
+                        audio.play();
+                        break;
+    
+                case 'r': var audio = new Audio("./sounds/kick-bass.mp3");
+                        audio.play();
+                        break;
+    
+                case 'a': var audio = new Audio("./sounds/snare.mp3");
+                        audio.play();
+                        break;
+                        
+                case 'w': var audio = new Audio("./sounds/tom-1.mp3");
+                        audio.play();
+                        break;
+                        
+                case 'i': var audio = new Audio("./sounds/tom-2.mp3");
+                        audio.play();
+                        break;
+                       
+                case 'n': var audio = new Audio("./sounds/tom-3.mp3");
+                        audio.play();
+                        break;
+                       
+                case 'c': var audio = new Audio("./sounds/tom-4.mp3");
+                        audio.play();
+                        break;
+                        
+                default:
+                    break;
+            }
+}
+
+function buttonAnimation(key){
+        var btn = document.querySelector("." +key);
+        btn.classList.add("pressed");
+
+        setTimeout(function() {
+                btn.classList.remove("pressed");
+        }, 100);
+}
